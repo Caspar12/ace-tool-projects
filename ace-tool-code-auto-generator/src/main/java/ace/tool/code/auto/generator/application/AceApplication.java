7e61414deece3877d61ffcb4acb591bc87827865
caspar <279397942@qq.com>
2020-07-24 10:12:47 +0800

    init project

--- ace-tool-code-auto-generator/src/main/java/ace/tool/code/auto/generator/application/AceApplication.java
+++ ace-tool-code-auto-generator/src/main/java/ace/tool/code/auto/generator/application/AceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * @author Caspar
